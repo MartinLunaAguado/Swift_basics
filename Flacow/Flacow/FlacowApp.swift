953b59198f8aec5661b6940b16a5f07f20785f6c
Martin Luna Aguado <luna@Mac-de-Martin.local>
2024-11-27 23:32:53 +0100

    primer commit

---
+++ Flacow/Flacow/FlacowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlacowApp.swift
+//  Flacow
+//
+//  Created by Martin Luna Aguado on 27/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FlacowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
